4921defc0b02dda3c4d621e2289890765b2eff7f
Adnan Alhassan <aditech5170@gmail.com>
2021-04-06 12:43:06 +0000

    Configure outbound for rapidpro flows (#156)
    * Configure outbound for rapidpro flows
    
    * Configure outbound for rapidpro flows
    
    * itech-malawi-outbound-for-rapidpro-flows
    
    * flow id updated
    
    * flow id updated
    
    * flow id updated

---
+++ itech-malawi/flows.js
@@ -0,0 +1,11 @@
+const RAPIDPRO_FLOWS = {
+  phone_owner: '7c46642d-8225-4d7f-a1a7-b2e3515792ae',
+  patient: '615bf7cb-8af2-48af-aaaf-698fbd3ee624',
+  write_patient_state: '6eca00f4-a371-49dc-9ce5-33750a88a3aa',
+
+  adherence: '59100883-c434-4595-a8e2-b39c42648b71',
+  check_in: 'a955e586-7f70-4186-ac79-6089231892a4',
+  reminders: '717a9798-8a5e-4827-bb7f-5d2af7d58f31'
+};
+
+module.exports = RAPIDPRO_FLOWS;
