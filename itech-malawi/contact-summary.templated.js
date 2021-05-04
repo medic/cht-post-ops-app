dd0b3649db6be8647277d5ffa09893f528abccac
femi oni <freefony@gmail.com>
2021-05-04 14:05:11 +0100

    Malawi/mpc number rewrite/236 (#237)
    * feat: add nation id filing number to enrollment form
    
    * feat: update contact summary card
    
    * feat: add new ids to edit person form
    
    * fix: next visit date

--- itech-malawi/contact-summary.templated.js
+++ itech-malawi/contact-summary.templated.js
@@ -29,9 +29,11 @@ const cards = [
 
             fields.push(
                 { label: 'Enrollment_date', value: thisContact.reported_date, filter: 'date', width: 6 },
-                { label: 'MPC N0', value: thisContact.mpc_no, width: 6 },
+                { label: 'National ID', value: thisContact.national_id, width: 6 },
+                { label: 'Filing Number', value: thisContact.filing_number, width: 6 },
                 { label: 'ARV N0', value: thisContact.arv_no, width: 6 },
-                { label: 'Preferred Language', value: thisContact.language_preference, width: 6, translate: true }
+                { label: 'Preferred Language', value: thisContact.language_preference, width: 6, translate: true },
+                { label: 'Next Visit Date', value: thisContact.rapidpro.visit_date, filter: 'date', width: 6 }
             );
 
             return fields;
@@ -40,39 +42,6 @@ const cards = [
             
             const latestVisitReport = getNewestReport(reports, ['date_change_request']);
             const currentVisitDate = getField(latestVisitReport, 'n.new_date');
-            // debugger;
-            // let lmpDate = getField(report, 'lmp_date_8601');
-            // let lmpMethodApprox = getField(report, 'lmp_method_approx');
-            // let hivTested = getField(report, 'hiv_status_known');
-            // let dewormingMedicationReceived = getField(report, 'deworming_med_received');
-            // let ttReceived = getField(report, 'tt_received');
-            // const riskFactorCodes = getAllRiskFactors(allReports, report);
-            // const riskFactorsCustom = getAllRiskFactorExtra(allReports, report);
-            // let pregnancyFollowupDateRecent = getField(report, 't_pregnancy_follow_up_date');
-      
-            // const followUps = getSubsequentPregnancyFollowUps(allReports, report);
-            // followUps.forEach(function (followUpReport) {
-            //   if (getField(followUpReport, 'lmp_updated') === 'yes') {
-            //     lmpDate = getField(followUpReport, 'lmp_date_8601');
-            //     lmpMethodApprox = getField(followUpReport, 'lmp_method_approx');
-            //   }
-            //   hivTested = getField(followUpReport, 'hiv_status_known');
-            //   dewormingMedicationReceived = getField(followUpReport, 'deworming_med_received');
-            //   ttReceived = getField(followUpReport, 'tt_received');
-            //   if (getField(followUpReport, 't_pregnancy_follow_up') === 'yes') { pregnancyFollowupDateRecent = getField(followUpReport, 't_pregnancy_follow_up_date'); }
-      
-            // });
-            // ctx.lmp_date_8601 = lmpDate;
-            // ctx.lmp_method_approx = lmpMethodApprox;
-            // ctx.is_active_pregnancy = true;
-            // ctx.deworming_med_received = dewormingMedicationReceived;
-            // ctx.hiv_tested_past = hivTested;
-            // ctx.tt_received_past = ttReceived;
-            // ctx.risk_factor_codes = riskFactorCodes.join(' ');
-            // ctx.risk_factor_extra = riskFactorsCustom.join('; ');
-            // ctx.pregnancy_follow_up_date_recent = pregnancyFollowupDateRecent;
-            // ctx.pregnancy_uuid = report._id;
-            // console.log(currentVisitDate, report);
             ctx.current_visit_date = currentVisitDate;
           }
     },
