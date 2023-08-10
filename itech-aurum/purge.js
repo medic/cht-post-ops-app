be47a8974ed69fa89d67904208243b1f0389a956
Femi Oni <freefony@gmail.com>
2023-08-10 08:57:25 +0100

    fix: purge reports after 45 days

--- itech-aurum/purge.js
+++ itech-aurum/purge.js
@@ -2,17 +2,17 @@ module.exports = {
   text_expression: 'at 12 am on Monday',
   run_every_days: 2,
   cron: '0 0 * * MON',
-  fn: function(userCtx, contact, reports, messages) {
-        const old = Date.now() - (1000 * 60 * 60 * 24 * 90);
-        const oldMessages = Date.now() - (1000 * 60 * 60 * 24 * 90);
-      
-        const reportsToPurge = reports
-                                 .filter(r => r.reported_date < old)
-                                 .map(r => r._id);
-        const messagesToPurge = messages
-                                 .filter(m => m.reported_date < oldMessages)
-                                 .map(m => m._id);
-      
-        return [...reportsToPurge, ...messagesToPurge];
-      } 
+  fn: function (userCtx, contact, reports, messages) {
+    const old = Date.now() - (1000 * 60 * 60 * 24 * 45);
+    const oldMessages = Date.now() - (1000 * 60 * 60 * 24 * 45);
+
+    const reportsToPurge = reports
+      .filter(r => r.reported_date < old)
+      .map(r => r._id);
+    const messagesToPurge = messages
+      .filter(m => m.reported_date < oldMessages)
+      .map(m => m._id);
+
+    return [...reportsToPurge, ...messagesToPurge];
+  }
 };
